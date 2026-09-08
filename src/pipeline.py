02cf57ba68d2f5922c20f39df286d162387b0f37
kmkan <kandpalkbusiness@gmail.com>
2026-09-07 23:49:35 -0400

    Create file to run all steps of the ETL pipeline.

---
+++ src/pipeline.py
@@ -0,0 +1,15 @@
+from etl.fetch import fetch_all
+from etl.chunk import process_article
+from etl.embed import embed_chunks
+from etl.store import store_chunks, get_collection
+
+def run():
+    articles = fetch_all()
+    chunks = []
+    
+    for article in articles:
+        chunks.extend(process_article(article))
+        
+    embedded = embed_chunks(chunks)
+    stored = store_chunks(embedded)
+    print(f'Pipeline ran successfully. Stored {stored} chunks. Chunks in collection: {get_collection().count()}.')
