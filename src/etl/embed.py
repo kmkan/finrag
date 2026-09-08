8154022327ef9b7015d52a0c12d06c0d48c82f12
kmkan <kandpalkbusiness@gmail.com>
2026-09-07 23:26:02 -0400

    Create module for embeddings step.

---
+++ src/etl/embed.py
@@ -0,0 +1,32 @@
+from sentence_transformers import SentenceTransformer
+from fetch import fetch_all
+from chunk import process_article
+
+MODAL = None
+
+def get_model():
+    global MODAL
+    if MODAL == None:
+        MODAL = SentenceTransformer('all-MiniLM-L6-v2')
+    return MODAL
+
+def embed_chunks(chunks):
+    model = get_model()
+    texts = [chunk['chunk_text'] for chunk in chunks]
+
+    embeddings = model.encode(texts)
+
+    for chunk, embedding in zip(chunks, embeddings):
+        chunk['embedding'] = embedding.tolist()
+
+    return chunks
+
+if __name__ == '__main__':
+    articles = fetch_all()
+    chunks = []
+    for article in articles:
+        chunks.extend(process_article(article))
+
+    embeddings = embed_chunks(chunks)
+    print(f'There are {len(embeddings)} embeddings.')
+    print(f'Examples: {embeddings[0]['embedding'][:5]}')
